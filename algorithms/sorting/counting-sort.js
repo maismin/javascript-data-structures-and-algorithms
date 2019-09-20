9ea8fcf63b050a4c213ae265aef73fdde590b5f9
Simon Mai <maismin@gmail.com>
2019-09-20 13:08:14 -0400

    Implement stable version of counting sort

--- algorithms/sorting/counting-sort.js
+++ algorithms/sorting/counting-sort.js
@@ -1,4 +1,5 @@
 // Assumes that the elements in arr are in the range from 0 to k
+// Not stable
 const countingSort = (arr, k) => {
   const sorted = []
   const freqCount = new Array(k + 1).fill(0)
@@ -12,6 +13,30 @@ const countingSort = (arr, k) => {
       sorted.push(idx)
       freqCount[idx] -= 1
     }
+  }
+
+  return sorted
+}
+
+// Stable version of counting sort
+const countingSortStable = (arr, k) => {
+  const sorted = []
+  const freqCount = new Array(k + 1).fill(0)
+
+  // freqCount[i] contains number of elements equal to i
+  for (let value of arr) {
+    freqCount[value] += 1
+  }
+
+  // freqCount[i] contains number of elements less than or equal to i
+  for (let i = 1; i < freqCount.length; i++) {
+    freqCount[i] += freqCount[i - 1]
+  }
+
+  // Loop through elements from array arr backwards and assign based on freqCount
+  for (let j = arr.length - 1; j >= 0; j--) {
+    sorted[freqCount[arr[j]] - 1]  = arr[j]
+    freqCount[arr[j]] -= 1
   }
 
   return sorted
